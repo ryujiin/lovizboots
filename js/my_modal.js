bff4416138f08dbbcf920167b20d12711228fee7
ryujiin <ryujiin22@gmail.com>
2016-10-19 22:23:36 -0500

    Estamos

--- js/my_modal.js
+++ js/my_modal.js
@@ -1,10 +1,16 @@
 (function($) {
 	Drupal.behaviors.miconsole = {
 		attach: function (context) {
-			var cuerpo = $('.cuerpo_modal .pane-content');			
-			var modal_pri = '<div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-body"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
-			var modal_segun = '</div></div></div>';
-			console.log('kike');
+			var cuerpo = $('.cuerpo_modal .pane-content').html();			
+			var modal_pri = '<div class="modal-dialog" role="document">
+								<div class="modal-content">
+									<div class="modal-body">
+										<div class="modal-header">
+									        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+									        <h4 class="modal-title" id="myModalLabel">Mi Revision del Producto</h4>
+									    </div>
+										<div class="pane-content">';
+			var modal_segun = '</div></div></div></div>';
 			$('.cuerpo_modal').html(modal_pri+cuerpo+modal_segun);
 		}
 	}
