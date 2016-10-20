1b319f41cb299d3a9ed295b54af7d9add3669213
ryujiin <ryujiin22@gmail.com>
2016-10-19 21:35:19 -0500

    Estamos agregando footer

--- js/my_modal.js
+++ js/my_modal.js
@@ -1,7 +1,14 @@
 (function($) {
 	Drupal.behaviors.miconsole = {
 		attach: function (context) {
-			console.log('kike');
+			debugger;
+			//var modal_pri = '<div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-body"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
+			//var modal_segun = '</div></div></div>';
+			//console.log('kike');
+			//$('.modal').append(modal_pri).prepend(modal_segun);
 		}
 	}
 })(jQuery);
+
+
+      
